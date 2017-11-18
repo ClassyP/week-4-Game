4562b46b2fd712b2b4a7c1f6e6d2dbe3a02e95e9
Jean Philip Lamarre <classyp@Jeans-MacBook-Pro.local>
2017-11-18 09:27:57 -0700

    updating the repo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,11 @@
-code
+
+// Setting global variables 
+var randomResult;
+var win;
+var lost;
+
+// My first function/for lood that will ittereat through the 4 crystals 
+for(var i = 0; i < 4; i++){
+
+    var crystal = $("")
+}
